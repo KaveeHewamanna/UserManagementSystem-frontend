69870c3aea198b702d17fbae60b4ad51b4908c4d
Kavee Hewamanna <hewamannakavee@gmail.com>
2025-01-25 21:03:15 +0530

    Create user registration form.

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -2,8 +2,60 @@ import React from 'react'
 
 export default function AddUser() {
   return (
-    <div>
-      <div>AddUser Form</div>
-    </div>
+    
+      <div className='container'>
+        <div className='row'> 
+            <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
+                <h2 className='text-cdenter m-4'>Register</h2>
+                <div className='mb-3'>
+                    <label htmlFor='Name' className='form-label'>
+                        Name
+                        </label> 
+                        <input 
+                        type={"text"}
+                        className='form-control'
+                        placeholder='Enter your name'
+                        name='name'>
+
+                        </input>
+
+                </div>
+
+                <div className='mb-3'>
+                    <label htmlFor='Username' className='form-label'>
+                        User Name
+                        </label> 
+                        <input 
+                        type={"text"}
+                        className='form-control'
+                        placeholder='Enter your username'
+                        name='name'>
+                            
+                        </input>
+
+                </div>
+
+                <div className='mb-3'>
+                    <label htmlFor='Email' className='form-label'>
+                        E-mail
+                        </label> 
+                        <input 
+                        type={"text"}
+                        className='form-control'
+                        placeholder='Enter your email address'
+                        name='email'>
+                            
+                        </input>
+
+                </div>
+<button type='submit' className='btn btn-outline-primary'>Submit</button>
+<button type='submit' className='btn btn-outline-danger mx-2'>Cancel</button>
+
+
+            </div>
+
+        </div>
+      </div>
+  
   )
 }
